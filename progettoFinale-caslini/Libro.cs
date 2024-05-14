3b3479f786540cce338c31405f5a2a8524c3f2ca
caslini.20353 <caslini.20353@E2-9-PC20>
2024-05-14 13:17:17 +0200

    Aggiungere i file di progetto.

---
+++ progettoFinale-caslini/Libro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace progettoFinale_caslini
+{
+    public class Libro
+    {
+        public string Titolo { get; set; }
+        public string Autore { get; set; }
+
+        public string Prezzo { get; set; }
+
+        public Libro(string titolo, string autore, string prezzo)
+        {
+            Titolo = titolo;
+            Autore = autore;
+            Prezzo = prezzo;
+
+        }
+    }
+}
